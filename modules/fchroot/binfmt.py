856851498e63efa2d3b9d58593c10abef43404dc
Daniel Robbins <drobbins@funtoo.org>
2019-03-06 12:18:07 -0700

    Latest work

--- modules/fchroot/binfmt.py
+++ modules/fchroot/binfmt.py
@@ -2,6 +2,32 @@
 
 import os, string, sys
 from .qemu import qemu_arch_settings
+from .exception import QEMUException, QEMUWrapperException
+
+# Where our stuff will look for qemu binaries:
+qemu_binary_path = "/usr/bin"
+
+# Where our code will try to store our compiled qemu wrappers:
+wrapper_storage_path = "/usr/share/fchroot/wrappers"
+
+def native_arch_desc():
+	uname_arch = os.uname()[4]
+	if uname_arch in ["x86_64", "AMD64"]:
+		host_arch = "x86-64bit"
+	elif uname_arch in ["x86", "i686", "i386"]:
+		host_arch = "x86-32bit"
+	else:
+		raise QEMUException("Arch of %s not recognized." % uname_arch)
+	return host_arch
+
+
+def supported_binfmts(native_arch_desc=None):
+	if native_arch_desc is None:
+		return set(qemu_arch_settings.keys())
+	else:
+		# TODO: return supported QEMU arch_descs specific to a native arch_desc.
+		return set()
+
 
 def get_binary_hexstring(path):
 	chunk_as_hexstring = ""
@@ -11,41 +37,41 @@ def get_binary_hexstring(path):
 	return chunk_as_hexstring
 
 
-printable_chars = set(string.printable)
-
-
 def escape_hexstring(hexstring):
-	global printable_chars
 	to_process = hexstring
 	to_output = ""
 	while len(to_process):
 		ascii_value = chr(int(to_process[:2], 16))
 		to_process = to_process[2:]
-		if ascii_value in printable_chars:
+		if ascii_value in set(string.printable):
 			to_output += ascii_value
 		else:
 			to_output += "\\x" + "{0:02x}".format(ord(ascii_value))
 	return to_output
 
 
-def configure_binfmt(arch_desc, wrapper_bin):
+def is_binfmt_registered(arch_desc):
+	return os.path.exists("/proc/sys/fs/binfmt_misc/" + arch_desc)
+
+
+def register_binfmt(arch_desc, wrapper_bin):
 	if not os.path.exists(wrapper_bin):
-		sys.stderr.write("Error: wrapper binary %s not found.\n" % wrapper_bin)
-		sys.exit(1)
+		raise QEMUWrapperException("Error: wrapper binary %s not found.\n" % wrapper_bin)
 	if arch_desc not in qemu_arch_settings:
-		sys.stderr.write("Error: arch %s not recognized. Specify one of: %s.\n" % (arch_desc, ", ".join(arch_dict.keys())))
-		sys.exit(1)
+		raise QEMUWrapperException("Error: arch %s not recognized. Specify one of: %s.\n" % (arch_desc, ", ".join(supported_binfmts())))
 	if os.path.exists("/proc/sys/fs/binfmt_misc/%s" % arch_desc):
-		sys.stderr.write("Error: binary format %s already registered in /proc/sys/fs/binfmt_misc.\n" % arch_desc)
-		sys.exit(1)
-	with open("/proc/sys/fs/binfmt_misc/register", "w") as f:
-		chunk_as_hexstring = qemu_arch_settings[arch_desc]['hexstring']
-		mask_as_hexstring = "fffffffffffffffcfffffffffffffffffeffff"
-		mask = int(mask_as_hexstring, 16)
-		chunk = int(chunk_as_hexstring, 16)
-		out_as_hexstring = hex(chunk & mask)[2:]
-		f.write(":%s:M::" % arch_desc)
-		f.write(escape_hexstring(out_as_hexstring))
-		f.write(":")
-		f.write(escape_hexstring(mask_as_hexstring))
-		f.write(":/usr/local/bin/%s:\n" % os.path.basename(wrapper_bin))
+		raise QEMUWrapperException("Error: binary format %s already registered in /proc/sys/fs/binfmt_misc.\n" % arch_desc)
+	try:
+		with open("/proc/sys/fs/binfmt_misc/register", "w") as f:
+			chunk_as_hexstring = qemu_arch_settings[arch_desc]['hexstring']
+			mask_as_hexstring = "fffffffffffffffcfffffffffffffffffeffff"
+			mask = int(mask_as_hexstring, 16)
+			chunk = int(chunk_as_hexstring, 16)
+			out_as_hexstring = hex(chunk & mask)[2:]
+			f.write(":%s:M::" % arch_desc)
+			f.write(escape_hexstring(out_as_hexstring))
+			f.write(":")
+			f.write(escape_hexstring(mask_as_hexstring))
+			f.write(":/usr/local/bin/%s:\n" % os.path.basename(wrapper_bin))
+	except (IOError, PermissionError) as e:
+		raise QEMUWrapperException("Was unable to write to /proc/sys/fs/binfmt_misc/register.")
--- modules/fchroot/qemu.py
+++ modules/fchroot/qemu.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+import os
+from .binfmt import wrapper_storage_path, qemu_binary_path
+from .exception import QEMUWrapperException
+
+
 qemu_arch_settings = {
 	'arm-64bit': {
 		'qemu_binary': 'qemu-aarch64',
@@ -19,3 +24,38 @@ native_support = {
 	'arm-64bit': ['arm-32bit'],
 	'arm-32bit': []
 }
+
+
+def compile_wrapper(qemu_arch):
+	"""
+	Compiles a QEMU wrapper using gcc. Will raise QEMUWrapperException if any error is encountered along the way.
+	:param qemu_arch: arch to build for -- should be 'arm-64bit' or 'arm-32bit' at the moment.
+	:return: None
+	"""
+	wrapper_code = """#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char **argv, char **envp) {{
+	char *newargv[argc + 3];
+
+	newargv[0] = argv[0];
+	newargv[1] = "-cpu";
+	newargv[2] = "{qemu_cpu}";
+
+	memcpy(&newargv[3], &argv[1], sizeof(*argv) * (argc -1));
+	newargv[argc + 2] = NULL;
+	return execve("/usr/local/bin/{qemu_binary}", newargv, envp);
+}}
+	"""
+	qemu_wrapper_binary = "%s/%s" % (qemu_binary_path, qemu_arch_settings[qemu_arch]['qemu_binary'])
+
+	if not os.path.exists(qemu_wrapper_binary):
+		raise QEMUWrapperException("Please ensure that %s exists first." % qemu_wrapper_binary)
+	try:
+		with open(os.path.join(wrapper_storage_path, "qemu-%s-wrapper.c" % qemu_arch), "w") as f:
+			f.write(wrapper_code.format(**qemu_arch_settings[qemu_arch]))
+		retval = os.system("cd {wrapper_path}; gcc -static -O2 -s -o qemu-{qemu_arch}-wrapper qemu-{qemu_arch}-wrapper.c".format(wrapper_path=wrapper_storage_path, qemu_arch=qemu_arch))
+		if retval != 0:
+			raise QEMUWrapperException("Compilation failed.")
+	except (IOError, PermissionError) as e:
+		raise QEMUWrapperException(str(e))
