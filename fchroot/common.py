82f943019b4525b0a248609843b46b93b26a81b5
Daniel Robbins <drobbins@funtoo.org>
2022-05-19 17:00:26 -0600

    I am adding functionality to allow specifying additional bind mounts using the --bind=src:dest option, where src is an absolute path to bind-mount within the fchroot at dest. I'm adding this so that I can jettison the bind-mount management code in metro and just rely on fchroot to manage all of it. This is a lot safer and more convenient.

--- fchroot/common.py
+++ fchroot/common.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import subprocess
 import sys
 
@@ -38,6 +39,35 @@ def run_verbose(action, cmd_list, badval=None):
     return success
 
 
+def parse_binds(bind_args, newroot):
+    binds = {}
+    invalid_dest = ["/sys", "/dev", "/proc"]
+    for bindval in bind_args:
+        bindval = bindval[0]
+        colon = bindval.rfind(":")
+        if colon == -1:
+            die(f"Error: For --bind option, specify a colon separating both paths. ('--bind={bindval}')")
+        src = bindval[:colon]
+        src = src.rstrip("/")
+        dest = bindval[colon + 1:]
+        dest = dest.rstrip("/")
+        if dest.startswith("/"):
+            dest = dest.lstrip("/")
+        if not os.path.isabs(src):
+            die(f"Error: Source path '{src}' must be absolute. ('--bind={bindval}')")
+        if not os.path.isabs("/"+dest):
+            die(f"Error: Destination path '{dest}' does not appear valid. ('--bind={bindval}')")
+        dest_abs = os.path.join(newroot, dest)
+        if not os.path.exists(dest_abs):
+            die(f"Error: Destination path '{dest}' does not appear to exist within the chroot. ('--bind={bindval}')")
+        if not os.path.isdir(src):
+            die(f"Error: Could not find source for bind '{src}'. Must exist and be a directory. ('--bind={bindval}')")
+        if dest in invalid_dest:
+            die(f"Error. Bind destination '{dest}' is already managed by fchroot automatically. ('--bind={bindval}')")
+        binds[dest] = src
+    return binds
+
+
 def parse_args():
     ap = argparse.ArgumentParser()
     ap.add_argument('--version', action='version', version=f"fchroot {__version__} ({__codename__})")
@@ -46,7 +76,41 @@ def parse_args():
     ap.add_argument("--preserve-env", action="store_true", default=False,
                     help="Preserve the current environment settings rather than wiping them by default.")
     ap.add_argument("--cpu", action="store", default=None, help="Specify specific CPU type for QEMU to use")
-    return ap.parse_known_args()
+    ap.add_argument("--bind", action="append", nargs=1, default=[], help="Specify additional bind mounts in src:dest format")
+    known, remainder = ap.parse_known_args()
+    binds = parse_binds(known.bind, known.newroot)
+    return known, binds, remainder
 
 
-args, commands = parse_args()
+args, local_binds, commands = parse_args()
+
+
+def bind_mount(chroot_path, umount=False):
+    global_binds = {
+        "proc": "/proc",
+        "sys": "/sys",
+        "dev": "/dev"
+    }
+
+    for recurse, bind_groups in (True, global_binds), (False, local_binds):
+        for src, dest in bind_groups.items():
+            dest_abs = os.path.join(chroot_path, dest)
+            if umount:
+                action = "umount"
+                mount_cmd = ["/bin/umount", "-R", "--lazy", dest_abs]
+                badval = 10
+            else:
+                action = "mount"
+                if not os.path.isdir(dest_abs):
+                    die(f"Required chroot directory '{dest_abs}' does not exist. Exiting.")
+                if recurse:
+                    bindopt = "--rbind"
+                else:
+                    bindopt = "--bind"
+                mount_cmd = ["/bin/mount", bindopt, f"/{src}", dest_abs]
+                badval = None
+            if action == "mount" and os.path.ismount(dest_abs):
+                sys.stderr.write(GREEN + f" {action}: /{dest} (already mounted)\n")
+            else:
+                run_verbose(action, mount_cmd, badval=badval)
+
