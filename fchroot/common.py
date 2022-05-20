1846ae74d048311e9454c5f19f3ba92be579c604
Daniel Robbins <drobbins@funtoo.org>
2022-05-19 20:08:28 -0600

    Add --nobind option to disable bind mounts entirely.

--- fchroot/common.py
+++ fchroot/common.py
@@ -77,7 +77,10 @@ def parse_args():
                     help="Preserve the current environment settings rather than wiping them by default.")
     ap.add_argument("--cpu", action="store", default=None, help="Specify specific CPU type for QEMU to use")
     ap.add_argument("--bind", action="append", nargs=1, default=[], help="Specify additional bind mounts in src:dest format")
+    ap.add_argument("--nobind", acction="store_true", default=False, help="Disable all bind mounts when entering fchroot")
     known, remainder = ap.parse_known_args()
+    if known.nobind and len(known.bind):
+        die(f"Error -- the --nobind and --bind arguments cannot be used at the same time.")
     binds = parse_binds(known.bind, known.newroot)
     return known, binds, remainder
 
